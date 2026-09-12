328b9d1a1007da094e61e86494b75edb30e41361
Badgio0906 <romcreater001@gmail.com>
2026-09-12 16:12:43 +0900

    Embed Japanese font for web rendering

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1789196786|2760868';
+const CACHE_VERSION = '1789197155|2662007';
 /** @type {string} */
 const CACHE_PREFIX = 'ウンコかウコンかゲーム-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
